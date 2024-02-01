92b232bb078a38db8d071dbe9a580884049ac023
Blerton Ismaili <ismaili.blerton.03@gmail.com>
2024-02-01 22:59:56 +0100

    Added heatmap & bar plot

--- Visualize.py
+++ Visualize.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 
 
@@ -31,4 +32,51 @@ def overall_scatter_plot(df):
     sns.scatterplot(x='date', y='value', data=df)
     plt.tick_params(axis='x', rotation=30)
     plt.title('Overall average temperature visualized with scatter plot')
+    plt.show()
+
+
+def heatmap(df):
+    del df['date']
+    del df['name']
+    df['temp_group'] = pd.cut(df['value'], bins=range(0, 16, 5), right=False)
+    df.to_csv('heatmap.csv')
+    # Determine temperature bins dynamically
+    min_temp = df['value'].min()
+    max_temp = df['value'].max()
+    bin_size = 5
+    temp_bins = range(int(min_temp // bin_size) * bin_size, int((max_temp // bin_size) + 1) * bin_size, bin_size)
+
+    # Create temperature groups
+    df['temp_group'] = pd.cut(df['value'], bins=temp_bins, right=False)
+
+    # Create a pivot table for the heatmap
+    heatmap_data = df.pivot_table(index='elevation', columns='temp_group', aggfunc=len, fill_value=0, observed=True)
+
+    # Create the heatmap using seaborn
+    sns.heatmap(heatmap_data, cmap='viridis', annot=True, fmt='g', cbar_kws={'label': 'Count'})
+    plt.title('Heatmap of Elevation and Temperature Groups')
+    plt.xlabel('Temperature Groups (℃)')
+    plt.ylabel('Elevation')
+    plt.show()
+
+
+def __mode(group):
+    return group.mode().iloc[0] if not group.mode().empty else 0
+
+
+def bar_plot(df):
+    mean_df = df.groupby('name')['value'].mean().reset_index()
+    mean_df['datatype'] = 'mean'
+    mode_df = df.groupby('name')['value'].agg(__mode).reset_index()
+    mode_df['datatype'] = 'mode'
+    median_df = df.groupby('name')['value'].median().reset_index()
+    median_df['datatype'] = 'median'
+    min_df = df.groupby('name')['value'].min().reset_index()
+    min_df['datatype'] = 'min'
+    max_df = df.groupby('name')['value'].max().reset_index()
+    max_df['datatype'] = 'max'
+    df = pd.concat([mean_df, mode_df, median_df, min_df, max_df])
+    sns.barplot(x='name', y='value', hue='datatype', data=df)
+    plt.tick_params(axis='x', rotation=30)
+    plt.title('Station mean, mode, median, min, max visualized with bar plot')
     plt.show()
--- main.py
+++ main.py
@@ -10,4 +10,9 @@ api = GHCND('https://www.ncei.noaa.gov/cdo-web/api/v2/data', os.getenv('API_KEY'
 df = api.make_request(locationid='FIPS:MK', startdate='2023-12-01', enddate='2023-12-30',
                       units='metric', limit='1000', offset='1')
 
-plot.station_scatter_plot(df)
+# plot.station_line_plot(df)
+# plot.overall_line_plot(df)
+# plot.station_scatter_plot(df)
+# plot.overall_scatter_plot(df)
+# plot.heatmap(df)
+plot.bar_plot(df)
